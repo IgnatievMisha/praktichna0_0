4093455a3117b8ada6f22facea5d71c89691d2f8
IgnatievMisha <mishaross1604@gmail.com>
2023-05-02 20:05:39 +0300

    blablabla

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#1
 def my_decorator(func):
     def wrapper():
         return func()+10
@@ -7,3 +8,19 @@ def example_function():
     n=int(input())
     return n
 print(example_function())
+#2
+import time
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        stat_time=time.time()
+        result = func(*args, **kwargs)
+        end_time =time.time()
+        print(end_time - stat_time)
+        return result
+    return wrapper
+@decorator
+def example_function(x):
+    time.sleep(1)
+    return x
+print(example_function(777))
+#3
